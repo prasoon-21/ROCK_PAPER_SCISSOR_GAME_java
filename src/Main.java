2f99cc887208f9a4254d67ce3292635664f94264
lokesh59 <lsharma4226@gmail.com>
2026-06-11 02:52:21 +0530

    Rock to Stone

--- src/Main.java
+++ src/Main.java
@@ -10,13 +10,13 @@ public class Main{
         String name = xy.nextLine();
         System.out.println("Welcome there!! "+ name);
         System.out.println("Lets Begin!! ");
-        String[] option = {"Prasun", "Paper", "Scissor"};
+        String[] option = {"Stone", "Paper", "Scissor"};
         String playAgain = " ";
         do {
-            System.out.println("Enter your move (Prasun, PAPER, SCISSOR)");
+            System.out.println("Enter your move (Stone, PAPER, SCISSOR)");
             String userMove = xy.next();
-            if(!userMove.equalsIgnoreCase("Prasun") && !userMove.equalsIgnoreCase("Paper") && !userMove.equalsIgnoreCase("Scissor")) {
-                System.out.println("INVALID CHOICE MADE !! Please enter the valid option (ROCK, PAPER,SCISSOR)");
+            if(!userMove.equalsIgnoreCase("Stone") && !userMove.equalsIgnoreCase("Paper") && !userMove.equalsIgnoreCase("Scissor")) {
+                System.out.println("INVALID CHOICE MADE !! Please enter the valid option (Stone, PAPER,SCISSOR)");
                 continue;
             }
             int index = xyz.nextInt(3);
